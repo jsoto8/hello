637831b75a6d96514954e7d2edd39d7f475caf12
jsoto8 <69829519+jsoto8@users.noreply.github.com>
2020-08-24 21:14:37 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Chris Bourke
+ *         cbourke@cse.unl.edu
+ * Date: 2020/03/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jehron Soto!\n");
+  printf("Computer Science and Mathematics\n");
+
+  return 0;
+}
